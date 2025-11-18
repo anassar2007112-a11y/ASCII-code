5d0a15e24cf3e6d9ecd072e60faf8d694e2dbb14
Ahmed_medhat <anassar2007112@gmail.com>
2025-11-18 15:14:24 +0200

    Add files via upload

---
+++ FileName.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+	char character;
+	cout << "Enter a character:";
+	cin >> character;
+	cout << "The ASCII value is:" << int(character) << endl;
+
+
+
+	return 0;
+}
